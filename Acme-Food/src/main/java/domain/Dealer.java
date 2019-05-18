80edb9d2dd15e0495d32e1264dda0f76114aa64a
Raul <raulrodriguezmendez97@gmail.com>
2019-05-18 09:54:56 +0200

    update models

--- Acme-Food/src/main/java/domain/Dealer.java
+++ Acme-Food/src/main/java/domain/Dealer.java
@@ -4,6 +4,9 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
@@ -11,8 +14,20 @@ import org.hibernate.validator.constraints.Range;
 @Access(AccessType.PROPERTY)
 public class Dealer extends Actor {
 
-	private int	isDealing;
+	private int			isDealing;
+	private Restaurant	restaurant;
 
+
+	@ManyToOne(optional = false)
+	@Valid
+	@NotNull
+	public Restaurant getRestaurant() {
+		return this.restaurant;
+	}
+
+	public void setRestaurant(final Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
 
 	@Range(min = 0, max = 1)
 	public int getIsDealing() {
