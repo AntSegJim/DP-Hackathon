aa8f1b21d8958398d7e78f9bf9760b30d67a600f
crilorbre <43986572+crilorbre@users.noreply.github.com>
2019-05-18 16:31:26 +0200

    update

--- Acme-Food/src/main/java/repositories/FoodDishesRepository.java
+++ Acme-Food/src/main/java/repositories/FoodDishesRepository.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import domain.FoodDishes;
 
+@Repository
 public interface FoodDishesRepository extends JpaRepository<FoodDishes, Integer> {
 
 	@Query("select f from FoodDishes f where f.restaurant.id = ?1")
