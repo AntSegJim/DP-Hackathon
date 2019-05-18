27266e46159300e08cd4dd6db6aa9779687b3574
Raul <raulrodriguezmendez97@gmail.com>
2019-05-18 12:31:39 +0200

    update

--- Acme-Food/src/main/java/services/OfferService.java
+++ Acme-Food/src/main/java/services/OfferService.java
@@ -7,11 +7,16 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
 import repositories.OfferRepository;
 import repositories.RestaurantRepository;
+import security.LoginService;
 import security.UserAccount;
 import domain.Offer;
+import domain.Restaurant;
 
 @Service
 @Transactional
@@ -23,6 +28,8 @@ public class OfferService {
 	private RestaurantRepository	restaurantRepository;
 	@Autowired
 	private ActorService			actorS;
+	@Autowired
+	private Validator				validator;
 
 
 	public Offer create() {
@@ -37,6 +44,51 @@ public class OfferService {
 
 	public Collection<Offer> findAll() {
 		return this.offerRepository.findAll();
+	}
+
+	public Offer findOne(final int offerId) {
+		final Offer offer = this.offerRepository.findOne(offerId);
+		final UserAccount userAccount = LoginService.getPrincipal();
+		final Restaurant restaurant = this.restaurantRepository.getRestaurantByUserAccount(userAccount.getId());
+		Assert.isTrue(userAccount.getAuthorities().iterator().next().getAuthority().equals("RESTAURANT"));
+		Assert.isTrue(offer.getRestaurant() == restaurant);
+		return offer;
+	}
+
+	public Offer save(final Offer offer) {
+		final UserAccount userAccount = LoginService.getPrincipal();
+		Assert.isTrue(userAccount.getAuthorities().iterator().next().getAuthority().equals("RESTAURANT"));
+		Assert.isTrue(offer.getRestaurant().equals(this.restaurantRepository.getRestaurantByUserAccount(userAccount.getId())));
+		final Offer offerSave = this.offerRepository.save(offer);
+		return offerSave;
+	}
+
+	public void delete(final Offer offer) {
+		final UserAccount userAccount = LoginService.getPrincipal();
+		Assert.isTrue(userAccount.getAuthorities().iterator().next().getAuthority().equals("RESTAURANT"));
+		Assert.isTrue(offer.getRestaurant().equals(this.restaurantRepository.getRestaurantByUserAccount(userAccount.getId())));
+	}
+
+	public Offer reconstruct(final Offer offer, final BindingResult binding) {
+		Offer res;
+		if (offer.getId() == 0) {
+			res = offer;
+			final UserAccount user = LoginService.getPrincipal();
+			final Restaurant restaurant = this.restaurantRepository.getRestaurantByUserAccount(user.getId());
+			res.setRestaurant(restaurant);
+			this.validator.validate(res, binding);
+		} else {
+			res = this.offerRepository.findOne(offer.getId());
+			final Offer a = new Offer();
+			a.setId(res.getId());
+			a.setVersion(res.getVersion());
+			a.setTitle(offer.getTitle());
+			a.setTotalPrice(offer.getTotalPrice());
+			a.setRestaurant(res.getRestaurant());
+			this.validator.validate(a, binding);
+			res = a;
+		}
+		return res;
 	}
 
 }
--- Acme-Food/src/main/java/services/SocialProfileService.java
+++ Acme-Food/src/main/java/services/SocialProfileService.java
@@ -6,10 +6,13 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.RestaurantRepository;
 import repositories.SocialProfileRepository;
+import security.LoginService;
 import security.UserAccount;
+import domain.Restaurant;
 import domain.SocialProfile;
 
 @Service
@@ -37,6 +40,23 @@ public class SocialProfileService {
 
 	public Collection<SocialProfile> findAll() {
 		return this.socialProfileRepository.findAll();
+	}
+
+	public SocialProfile findOne(final int socialProfileId) {
+		final SocialProfile socialProfile = this.socialProfileRepository.findOne(socialProfileId);
+		final UserAccount userAccount = LoginService.getPrincipal();
+		final Restaurant restaurant = this.restaurantRepository.getRestaurantByUserAccount(userAccount.getId());
+		Assert.isTrue(userAccount.getAuthorities().iterator().next().getAuthority().equals("RESTAURANT"));
+		Assert.isTrue(socialProfile.getRestaurant() == restaurant);
+		return socialProfile;
+	}
+
+	public SocialProfile save(final SocialProfile socialProfile) {
+		final UserAccount userAccount = LoginService.getPrincipal();
+		Assert.isTrue(userAccount.getAuthorities().iterator().next().getAuthority().equals("RESTAURANT"));
+		Assert.isTrue(socialProfile.getRestaurant().equals(this.restaurantRepository.getRestaurantByUserAccount(userAccount.getId())));
+		final SocialProfile socialProfileSave = this.socialProfileRepository.save(socialProfile);
+		return socialProfileSave;
 	}
 
 }
