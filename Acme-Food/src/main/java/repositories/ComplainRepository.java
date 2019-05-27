1112c951460b8c26dbef149ae3d41652c0507f8e
crilorbre <43986572+crilorbre@users.noreply.github.com>
2019-05-27 21:33:30 +0200

    Dashboard

--- Acme-Food/src/main/java/repositories/ComplainRepository.java
+++ Acme-Food/src/main/java/repositories/ComplainRepository.java
@@ -15,4 +15,8 @@ public interface ComplainRepository extends JpaRepository<Complain, Integer> {
 	@Query("select c from Complain c where c.customer.id=?1")
 	public Collection<Complain> getComplainByCustomer(Integer customerId);
 
+	//DashBoard
+	@Query("select count(r)*1.0/(select count(x) from Restaurant x) from Restaurant r where (select count(c) from Complain c where c.cashOrder.restaurant.id =r.id) > 0")
+	public Double ratioOfRestaurantsWithComplain();
+
 }
